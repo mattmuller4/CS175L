714d159db0c095c77cfaf3df6da101c2aa491110
mattmuller4 <157052381+mattmuller4@users.noreply.github.com>
2024-02-12 12:46:03 -0500

    Add files via upload
    restaurant version 2

--- Restuarant assignment.py
+++ Restuarant assignment.py
@@ -1,38 +1,42 @@
-vegetarian = input('Is anyone in your party a vegetarian? ')
-vegan = input('Is anyone in your party a vegan? ')
-gluten_free = input('Is anyone in your party gluten free? ')
-
-print('Here are your restuarant choices: ')
-
-if vegetarian == 'Yes' and vegan == 'Yes' and gluten_free == 'Yes':
-    print("The Chef's Kitchen")
-    print('The Corner Cafe')
-elif vegetarian == 'No' and vegan == 'Yes' and gluten_free == 'Yes':
-    print('The Corner Cafe')
-    print("The Chef's Kitchen")
-elif vegetarian == 'No' and vegan == 'No' and gluten_free == 'Yes':
-    print('Main Street Pizza Company')
-    print('Corner Cafe')
-    print("The Chef's Kitchen")
-elif vegetarian == 'No' and vegan == 'No' and gluten_free == 'No':
-    print("Joe's Gourmet Burgers")
-    print('Main Street Pizza Company')
-    print('Corner Cafe')
-    print("Mama's Fine Italian")
-    print("The Chef's Kitchen")
-elif vegetarian == 'Yes' and vegan == 'No' and gluten_free == 'Yes':
-    print('Main Street Pizza Company')
-    print('Corner Cafe')
-    print("The Chef's Kicthen")
-elif vegetarian == 'Yes' and vegan == 'No' and gluten_free == 'No':
-    print('Main Street Pizza Company')
-    print('Corner Cafe')
-    print("Mama's Fine Italian")
-    print("The Chef's Kitchen")
-elif vegetarian == 'Yes' and vegan == 'Yes' and gluten_free == 'No':
-    print('Corner Cafe')
-    print("The Chef's Kitchen")
-elif vegetarian == 'No' and vegan == 'Yes' and gluten_free == 'No':
-    print('Corner Cafe')
-    print("The Chef's Kitchen")
-    
+#CS 175L
+#Matthew Muller
+#Restuarants
+keep_going = 'y'
+while keep_going == 'y':
+    vegetarian = input('Is anyone in your party a vegetarian? ')
+    vegan = input('Is anyone in your party a vegan? ')
+    gluten_free = input('Is anyone in your party gluten free? ')
+    print('Here are your restuarant choices: ')
+    if vegetarian == 'Yes' and vegan == 'Yes' and gluten_free == 'Yes':
+        print("The Chef's Kitchen")
+        print('The Corner Cafe')
+    elif vegetarian == 'No' and vegan == 'Yes' and gluten_free == 'Yes':
+        print('The Corner Cafe')
+        print("The Chef's Kitchen")
+    elif vegetarian == 'No' and vegan == 'No' and gluten_free == 'Yes':
+        print('Main Street Pizza Company')
+        print('Corner Cafe')
+        print("The Chef's Kitchen")
+    elif vegetarian == 'No' and vegan == 'No' and gluten_free == 'No':
+        print("Joe's Gourmet Burgers")
+        print('Main Street Pizza Company')
+        print('Corner Cafe')
+        print("Mama's Fine Italian")
+        print("The Chef's Kitchen")
+    elif vegetarian == 'Yes' and vegan == 'No' and gluten_free == 'Yes':
+        print('Main Street Pizza Company')
+        print('Corner Cafe')
+        print("The Chef's Kicthen")
+    elif vegetarian == 'Yes' and vegan == 'No' and gluten_free == 'No':
+        print('Main Street Pizza Company')
+        print('Corner Cafe')
+        print("Mama's Fine Italian")
+        print("The Chef's Kitchen")
+    elif vegetarian == 'Yes' and vegan == 'Yes' and gluten_free == 'No':
+        print('Corner Cafe')
+        print("The Chef's Kitchen")
+    elif vegetarian == 'No' and vegan == 'Yes' and gluten_free == 'No':
+        print('Corner Cafe')
+        print("The Chef's Kitchen")
+    print('Here are your restuarant choices: ')
+    keep_going = input('Do you want to do another search?' + '(Enter y for yes)' + '(Enter n for end): ')
